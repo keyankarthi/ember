3ae0390e316b134b399e67d4f2129a830aff4e70
lnc <lnc>
2017-04-21 18:24:36 +0530

    Initial commit

---
+++ tests/unit/helpers/oops-test.js
@@ -0,0 +1,12 @@
+
+import { oops } from 'superrentel/helpers/oops';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | oops');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = oops([42]);
+  assert.ok(result);
+});
+
